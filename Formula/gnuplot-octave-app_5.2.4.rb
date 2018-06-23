079d69a655a1e0fa20d0bec71ffbc80aeb147637
Andrew Janke <andrew@apjanke.net>
2018-06-23 14:14:50 -0400

    Switch back to unpatched Qt to see if it fixes the test suite dialog hang

--- Formula/gnuplot-octave-app_5.2.4.rb
+++ Formula/gnuplot-octave-app_5.2.4.rb
@@ -30,7 +30,7 @@ class GnuplotOctaveApp524 < Formula
   depends_on "readline_7.0.3"
   depends_on "lua_5.3.4" => :recommended
   depends_on "pango" if build.with?("cairo") || build.with?("wxmac")
-  depends_on "qt-octave-app_5.11.1" => :recommended
+  depends_on "qt_5.11.1" => :recommended
   depends_on "wxmac_3.0.4" => :optional
   depends_on :x11 => :optional
 
