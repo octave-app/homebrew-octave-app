b71765652dc09d5241551e44606b4d35780e3795
Andrew Janke <andrew@apjanke.net>
2018-07-16 23:35:50 -0400

    add openjpeg_2.3.0

---
+++ Formula/openjpeg_2.3.0.rb
@@ -0,0 +1,46 @@
+class Openjpeg230 < Formula
+  desc "Library for JPEG-2000 image manipulation"
+  homepage "https://www.openjpeg.org/"
+  url "https://github.com/uclouvain/openjpeg/archive/v2.3.0.tar.gz"
+  sha256 "3dc787c1bb6023ba846c2a0d9b1f6e179f1cd255172bde9eb75b01f1e6c7d71a"
+  head "https://github.com/uclouvain/openjpeg.git"
+
+  
+
+  option "without-doxygen", "Do not build HTML documentation."
+  option "with-static", "Build a static library."
+
+  depends_on "cmake_3.11.4" => :build
+  depends_on "doxygen_1.8.14" => [:build, :recommended]
+  depends_on "little-cms2_2.9"
+  depends_on "libtiff_4.0.9"
+  depends_on "libpng_1.6.34"
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_SHARED_LIBS=OFF" if build.with? "static"
+    args << "-DBUILD_DOC=ON" if build.with? "doxygen"
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <openjpeg.h>
+
+      int main () {
+        opj_image_cmptparm_t cmptparm;
+        const OPJ_COLOR_SPACE color_space = OPJ_CLRSPC_GRAY;
+
+        opj_image_t *image;
+        image = opj_image_create(1, &cmptparm, color_space);
+
+        opj_image_destroy(image);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-I#{include.children.first}", "-L#{lib}", "-lopenjp2",
+           testpath/"test.c", "-o", "test"
+    system "./test"
+  end
+end
