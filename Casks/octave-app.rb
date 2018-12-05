bab0f011f721bd7ae6b46c41b81d3ac3c739badb
Andrew Janke <floss@apjanke.net>
2018-12-04 23:54:14 -0500

    Update Cask to 4.4.0 final

--- Casks/octave-app.rb
+++ Casks/octave-app.rb
@@ -2,7 +2,7 @@ cask 'octave-app' do
   version '4.4.0'
   sha256 '5b797f99f9e6ba028db3047783d63e87e2456001d3f98dd10403a762c0f7076c'
 
-  url "https://github.com/octave-app/octave-app/releases/download/v4.4.0-beta10/Octave-4.4.0-beta10.dmg"
+  url "https://github.com/octave-app/octave-app/releases/download/v4.4.0/Octave-4.4.0.dmg"
   name 'Octave'
   homepage 'https://octave-app.org'
 
@@ -14,12 +14,5 @@ cask 'octave-app' do
 
   caveats do
     depends_on_java '8'
-
-    <<~EOS
-    This is a BETA release! It may be buggy or unstable.
-
-    For help, please contact the Octave.app organization at https://octave-app.org.
-    Bug reports may be filed at https://github.com/octave-app/octave-app/issues.
-    EOS
   end
 end
