de2ac63ee15ceefdda1816341638dff0a754411e
Andrew Janke <floss@apjanke.net>
2019-01-28 09:27:48 -0500

    octave 4.4.0: munge HG-ID

--- Formula/octave-octave-app_4.4.0.rb
+++ Formula/octave-octave-app_4.4.0.rb
@@ -99,6 +99,11 @@ class OctaveOctaveApp440 < Formula
   cxxstdlib_check :skip
 
   def install
+    # Hack: munge HG-ID to reflect that we're adding patches
+    hg_id = `cat HG-ID`.chomp
+    File.delete("HG-ID")
+    Pathname.new("HG-ID").write "#{hg_id} + patches\n"
+
     # do not execute a test that may trigger a dialog to install java
     inreplace "libinterp/octave-value/ov-java.cc", "usejava (\"awt\")", "false ()"
 
