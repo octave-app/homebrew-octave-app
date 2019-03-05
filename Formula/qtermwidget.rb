9c3ec98c57c956c2754bad74302f5a52d8fd7b0a
Andrew Janke <floss@apjanke.net>
2019-03-05 04:52:27 -0500

    qtermwidget: bump to 0.14.1

--- Formula/qtermwidget.rb
+++ Formula/qtermwidget.rb
@@ -1,8 +1,8 @@
 class Qtermwidget < Formula
   desc "The terminal widget for QTerminal"
   homepage "http://lxqt.org"
-  url "https://github.com/lxqt/qtermwidget/releases/download/0.9.0/qtermwidget-0.9.0.tar.xz"
-  sha256 "e39ce62fec18112634630654f41f08f4be4638bcf0bebbc89d71c9aefdfa38b0"
+  url "https://github.com/lxqt/qtermwidget/releases/download/0.14.1/qtermwidget-0.14.1.tar.xz"
+  sha256 "84739f91e6ac5900a39ed7cbb254397a9428b172ee3fe0d1b6c827b751dc3b6c"
   head "https://github.com/lxqt/qtermwidget.git"
   
   depends_on "cmake" => :build
