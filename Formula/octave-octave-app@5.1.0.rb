25f7d598f44835d193f9f7d1dd1a7e493ff365d8
Andrew Janke <andrew@apjanke.net>
2019-05-05 14:33:53 -0400

    octave 5.1: temporarily revert to Qt 5.12 (plain "qt" formula) so I can reproduce brew issue

--- Formula/octave-octave-app@5.1.0.rb
+++ Formula/octave-octave-app@5.1.0.rb
@@ -61,8 +61,8 @@ class OctaveOctaveAppAT510 < Formula
 
   # Dependencies for the graphical user interface
   if build.with?("qt")
-    depends_on "qt_5.11"
-    depends_on "qscintilla2-qt511"
+    depends_on "qt"
+    depends_on "qscintilla2"
 
     # Fix bug #50025: Octave window freezes
     # see https://savannah.gnu.org/bugs/?50025
@@ -139,10 +139,10 @@ class OctaveOctaveAppAT510 < Formula
       # source hasn't been updated to auto-detect this yet.
       #ENV['QCOLLECTIONGENERATOR']='qhelpgenerator'
       # These "shouldn't" be necessary, but the build breaks if I don't include them.
-      ENV['QT_CPPFLAGS']="-I#{Formula["qt_5.11"].opt_include}"
-      ENV.append 'CPPFLAGS', "-I#{Formula["qt_5.11"].opt_include}"
-      ENV['QT_LDFLAGS']="-F#{Formula["qt_5.11"].opt_lib}"
-      ENV.append 'LDFLAGS', "-F#{Formula["qt_5.11"].opt_lib}"
+      ENV['QT_CPPFLAGS']="-I#{Formula["qt"].opt_include}"
+      ENV.append 'CPPFLAGS', "-I#{Formula["qt"].opt_include}"
+      ENV['QT_LDFLAGS']="-F#{Formula["qt"].opt_lib}"
+      ENV.append 'LDFLAGS', "-F#{Formula["qt"].opt_lib}"
     end
 
     if build.without? "docs"
@@ -184,7 +184,7 @@ class OctaveOctaveAppAT510 < Formula
         f.write("<?xml version=\"1.0\" encoding=\"utf-8\" ?>")
         f.write("<QHelpCollectionProject version=\"1.0\" />")
       end
-      system "#{Formula["qt_5.11"].opt_bin}/qhelpgenerator", "doc/octave_interpreter.qhcp", "-o", "doc/octave_interpreter.qhc"
+      system "#{Formula["qt"].opt_bin}/qhelpgenerator", "doc/octave_interpreter.qhcp", "-o", "doc/octave_interpreter.qhc"
       (pkgshare/"#{version}/doc").install "doc/octave_interpreter.qhc"
     end
   end
