d96217dd72aa3249e1a04e2fda08a8d52de3a2e9
Andrew Janke <andrew@apjanke.net>
2019-11-05 04:46:09 -0500

    octave-octave-app@5.1.0: re-enable docs; the failure was due to a Qt bogus-relocatable-bottle issue

--- Formula/octave-octave-app@5.1.0.rb
+++ Formula/octave-octave-app@5.1.0.rb
@@ -26,8 +26,7 @@ class OctaveOctaveAppAT510 < Formula
   keg_only "so it can be installed alongside regular octave"
 
   option "without-qt", "Compile without qt-based graphical user interface"
-  # Doco temporarily disabled because it's breaking the build
-  option "with-docs", "Skip documentation (documentation requires MacTeX)"
+  option "without-docs", "Skip documentation (documentation requires MacTeX)"
   option "with-test", "Do compile-time make checks"
 
   @qt_formula = "qt_5.13"
