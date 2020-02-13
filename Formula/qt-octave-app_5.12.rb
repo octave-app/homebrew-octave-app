625dc97fd58057babf17917216cc8e4841809679
Andrew Janke <andrew@apjanke.net>
2020-02-13 04:02:40 -0500

    Try bumping qt-octave-app from Qt 5.12 LTS to Qt 5.14 in hopes that it improves GUI freeziness

--- Formula/qt-octave-app_5.12.rb
+++ Formula/qt-octave-app_5.12.rb
@@ -1,4 +1,4 @@
-# Qt, hacked for Octave.app
+# Qt 5.12 LTS, hacked for Octave.app
 #
 # Our hack just suppresses an "FSEvents assertion failure" warning message. Nothing
 # else; a shame we have to pay for the whole Qt build for it.
@@ -6,14 +6,14 @@
 # This formula will track the version of Qt we're using for Octave.app builds, which
 # might be the LTS version, the latest version, or something else, depending on what
 # works best for Octave.app.
-class QtOctaveApp < Formula
+class QtOctaveApp512 < Formula
   desc "Cross-platform application and UI framework, Octave.app-hacked version"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/5.14/5.14.1/single/qt-everywhere-src-5.14.1.tar.xz"
-  mirror "https://qt.mirror.constant.com/archive/qt/5.14/5.14.1/single/qt-everywhere-src-5.14.1.tar.xz"
-  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/qt5/qt-everywhere-src-5.14.1.tar.xz"
-  sha256 "6f17f488f512b39c2feb57d83a5e0a13dcef32999bea2e2a8f832f54a29badb8"
-  head "https://code.qt.io/qt/qt5.git", :branch => "5.14", :shallow => false
+  url "https://download.qt.io/official_releases/qt/5.12/5.12.7/single/qt-everywhere-src-5.12.7.tar.xz"
+  mirror "https://qt.mirror.constant.com/archive/qt/5.12/5.12.7/single/qt-everywhere-src-5.12.7.tar.xz"
+  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/qt5/qt-everywhere-src-5.12.7.tar.xz"
+  sha256 "873783a0302129d98a8f63de9afe4520fb5f8d5316be8ad7b760c59875cd8a8d"
+  head "https://code.qt.io/qt/qt5.git", :branch => "5.12", :shallow => false
 
   keg_only "Qt 5 has CMake issues when linked"
 
