c04f8a531e46bbb23dbbf0861814d35508693cf1
Andrew Janke <andrew@apjanke.net>
2020-02-14 10:19:46 -0500

    Octave 5.2: fix bin symlink name

--- Formula/octave@5.2.0.rb
+++ Formula/octave@5.2.0.rb
@@ -174,7 +174,7 @@ class OctaveAT520 < Formula
   end
 
   def post_install
-    system "ln", "-sf", "#{bin}/octave", "#{HOMEBREW_PREFIX}/bin/octave@5.1.0"
+    system "ln", "-sf", "#{bin}/octave", "#{HOMEBREW_PREFIX}/bin/octave@5.2.0"
   end
 
   test do
