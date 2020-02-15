20b1d250fb3981894a693048da031f433e6eb368
Andrew Janke <andrew@apjanke.net>
2020-02-15 00:13:07 -0500

    WIP: start on a librsb formula

---
+++ Formula/librsb.rb
@@ -0,0 +1,16 @@
+class Librsb < Formula
+  desc "Recursive Sparse Blocks shared memory parallel sparce matrix library"
+  homepage "http://librsb.sourceforge.net/"
+  url "https://sourceforge.net/projects/librsb/files/librsb-1.2.0.8.tar.gz"
+  sha256 "8bebd19a1866d80ade13eabfdd0f07ae7e8a485c0b975b5d15f531ac204d80cb"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+end
