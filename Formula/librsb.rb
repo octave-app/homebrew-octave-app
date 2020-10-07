b2333eb9af55d8798828811938a44a6a5e75b2d9
Andrew Janke <andrew@apjanke.net>
2020-10-07 12:00:31 -0400

    librsb: explicitly link libomp (working now!)

--- Formula/librsb.rb
+++ Formula/librsb.rb
@@ -5,8 +5,10 @@ class Librsb < Formula
   sha256 "8bebd19a1866d80ade13eabfdd0f07ae7e8a485c0b975b5d15f531ac204d80cb"
 
   depends_on "libomp"
-  
+
   def install
+  	# Explicit -lomp is required on macOS
+  	ENV.append "LIBS", "-lomp"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
