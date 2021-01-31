5550a1e49cf0d2bc6f482b054ac1d6d853d25518
Andrew Janke <andrew@apjanke.net>
2021-01-31 16:54:55 -0500

    Octave 6.1.0 cask

--- Casks/octave-app.rb
+++ Casks/octave-app.rb
@@ -1,14 +1,14 @@
 cask 'octave-app' do
-  version '4.4.1'
-  sha256 'bb5928281e130b09798b106b04bec241898d7b927e8a5495ae041c60ccf7c779'
+  version '6.1.0'
+  sha256 'fe45f23e307e922e6ad75d3e4ef1b357b3e4d656205f659741986704f96a17f7'
 
-  url "https://github.com/octave-app/octave-app/releases/download/v4.4.1/Octave-4.4.1.dmg"
+  url "https://github.com/octave-app/octave-app/releases/download/v6.1.0/Octave-6.1.0.dmg"
   name 'Octave'
   homepage 'https://octave-app.org'
 
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: '>= :mojave'
 
   auto_updates false
 
-  app 'Octave-4.4.1.app'
+  app 'Octave-6.1.0.app'
 end
